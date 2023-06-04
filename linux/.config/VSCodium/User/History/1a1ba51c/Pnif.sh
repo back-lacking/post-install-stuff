f9e7ec9558983ad1cbc7b5872fabcfdf648eca45
back-lacking <86173810+back-lacking@users.noreply.github.com>
2023-06-04 14:52:29 -0400

    changed config again

---
+++ linux/.config/VSCodium/User/History/1a1ba51c/Pnif.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+$SYNCTHING_REPO_URL="https://github.com/syncthing/syncthing/releases/latest"
+$SYNCTHING_REDIRECT_URL=$(curl -s -I $SYNCTHING_REPO_URL -L | awk '/Location: (.*)/ {print $2}' | tail -n 1)
+$SYNCTHING_VERSION=$(echo $SYNCTHING_REDIRECT_URL | egrep "/(?<=/tag\/)(.*)(?=\/)/s")
+# url/downloads/blah/tag/$($SYNCTHING_VERSION)/downloadsorwhatever/$($SYNCTHING_VERSION)-linux-amd64-unicorns.7z
+wget https://github.com/syncthing/syncthing/releases/download/$($SYNCTHING_VERSION)/syncthing-linux-($($SYNCTHING_VERSION)).tar.gz --trust-server-names
