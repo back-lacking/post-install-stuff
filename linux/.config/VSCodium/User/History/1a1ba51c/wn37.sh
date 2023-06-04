67c5c7acb7874ffd57e6f426d1218fa368a91f5e
back-lacking <86173810+back-lacking@users.noreply.github.com>
2023-06-04 14:58:20 -0400

    yeeted configs

--- linux/.config/VSCodium/User/History/1a1ba51c/wn37.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-SYNCTHING_REPO_URL="https://github.com/syncthing/syncthing/releases/latest"
-SYNCTHING_REDIRECT_URL=$(curl -s -I $SYNCTHING_REPO_URL -L | awk '/Location: (.*)/ {print $2}' | tail -n 1)
-SYNCTHING_VERSION=$(echo $SYNCTHING_REDIRECT_URL | grep -E "/(?<=/tag\/)(.*)(?=\/)/s")
-# url/downloads/blah/tag/$($SYNCTHING_VERSION)/downloadsorwhatever/$($SYNCTHING_VERSION)-linux-amd64-unicorns.7z
-wget https://github.com/syncthing/syncthing/releases/download/$SYNCTHING_VERSION/syncthing-linux-$SYNCTHING_VERSION.tar.gz --trust-server-names
